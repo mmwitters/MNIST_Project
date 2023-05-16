f05b5a70c9d26869a2babd0234c249268bd49fd3
Madeline Witters <mmwitters@gmail.com>
2023-05-16 14:48:46 -0500

    Importing MNIST dataset

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.neural_network import MLPClassifier
+
+
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True, parser='auto')
+#dataset from: https://www.openml.org/search?type=data&sort=runs&id=554&status=active
+
+X = X / 255.
+
+# rescale the data, use the traditional train/test split
+X_train, X_test = X[:60000], X[60000:]
+y_train, y_test = y[:60000], y[60000:]
+
+as_arr = np.array(X.iloc[0])
+
+img_plt = plt.imshow(as_arr.reshape(28,28), cmap='gray')
