cb0d83dab79cfcbadba0f9f3577b6b8fd4007272
Madeline Witters <mmwitters@gmail.com>
2023-05-16 15:15:55 -0500

    Building MLP classifier

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import warnings
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -7,16 +9,24 @@ from sklearn.neural_network import MLPClassifier
 
 
 X, y = fetch_openml('mnist_784', version=1, return_X_y=True, parser='auto')
-#dataset from: https://www.openml.org/search?type=data&sort=runs&id=554&status=active
+#dataset from: https://www.openml.org/d/554
 
 X = X / 255.
-# rescale the data, use the traditional train/test split
+
 X_train, X_test = X[:60000], X[60000:]
 y_train, y_test = y[:60000], y[60000:]
 
 row_num = 45
 as_arr = np.array(X.iloc[row_num].values).reshape((28, 28))
-print(y.iloc[row_num])
+# print(y.iloc[row_num])
+#
+# plt.imshow(as_arr, cmap='gray')
+# plt.show()
 
-plt.imshow(as_arr, cmap='gray')
-plt.show()
+mlp = MLPClassifier(hidden_layer_sizes=(50,), max_iter=10, alpha=1e-4,
+                    solver='sgd', verbose=10, random_state=1,
+                    learning_rate_init=.1)
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=ConvergenceWarning, module="sklearn")
+    mlp.fit(X_train, y_train)
